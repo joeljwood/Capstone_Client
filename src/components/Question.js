90d75f535af83d1dbb4d5f089cd67c6c02ab7b32
Joel Wood <joeljwood@gmail.com>
2021-04-01 08:10:53 -0600

    Reformatted quetions into a form

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,40 +1,23 @@
-//import react from 'react'
-import react, {useEffect,useState} from 'react'
+
+import react, {useRef, useEffect,useState} from 'react'
 import * as Constants from '../constantsquestion'
 import axios from 'axios'
 import "../assets/style.css";
-import Review from './Review';
-import { Redirect } from "react-router-dom";
 
 function Question (props) {
     
+     const a1 =useRef(null);
+     const a2 =useRef(null);
+     const a3 =useRef(null);
+     const a4 =useRef(null);
+     const a5 =useRef(null);
+     const a6 =useRef(null);
+     const a7 =useRef(null);
      const [data,setData] = useState( { questions : []} );
-     const [currentquestion,setcurrentquestion] =useState(0);
-     const numberOfQuestions = data.questions.length -1;
-     const [answer, setAnswer] = useState([""]);
-     const score = 0;
+     const [fromData,setFormData] = useState({});
+     //const [answer, setAnswer] = useState([""]);
+     //const score = 0;
 
-     const next =() =>{
-      console.log({answer});
-      if(currentquestion < numberOfQuestions ){
-     const nextquestion=currentquestion +1;
-     setcurrentquestion(nextquestion);
-      }
-      else{
-        //setcurrentquestion(currentquestion);
-        <Route path="/review" component={Review}/>
-
-      }
-     };
-     const previous =() =>{
-       if(currentquestion > 0){
-      const previousquestion=currentquestion - 1;
-      setcurrentquestion(previousquestion);
-     }
-     else {
-      setcurrentquestion(currentquestion);
-     }
-      };
     useEffect(() => {
       const fetchData = async () => {
    //call graphql api
@@ -51,52 +34,39 @@ function Question (props) {
     fetchData();
     },[] )
 
-    function answersYes(){
-      console.log(data.questions[currentquestion].id);
-      
-      if(answer.length === data.questions[currentquestion].id){
-        console.log("here");
-        setAnswer(answer => [...answer, 
-          "yes"]);
-        }
-      else{
-        console.log("here");
-        answer[data.questions[currentquestion].id] = "yes";
-      }
-      
-      /*if(answer.length === data.questions[currentquestion].id){
-        setAnswer([answer.slice(data.questions[currentquestion].id,1,"yes")]);
-      }*/
-      
-      //set answer array to contain current question
-      
-      //call method that changes data answered question to yes
-      //axios.post(
-      //  Constants.GRAPHQL_API, {
-      //    MutationEvent:Constants.SET_ANSWER_MUTILATION
-      //}
-      //);
-      };
-
-    function answersNo(){
-      
-      //call method that changes data answered question to no
-      console.log(data.questions[currentquestion].id);
-      if(answer.length === data.questions[currentquestion].id){
-        setAnswer(answer => [...answer, 
-          "no"]);
-      }else{
-        console.log("here");
-        answer[data.questions[currentquestion].id] = "no";
-      }
-
-      console.log({answer});
-      //const yes= await.axios.post(
-      //  Constants.GRAPHQL_API, {
-      //    mutilation:Constants.SET_ANSWER_MUTILATION
-      //}
-      //);
-      };
+    let formElements =[{
+      labelid: 0,
+      key: 'answer1',
+      reference: a1
+    },{
+      labelid: 1,
+      key: 'answer2',
+      reference: a2
+    },{
+      labelid: 2,
+      key: 'answer3',
+      reference: a3
+    },
+    {
+      labelid: 3,
+      key: 'answer4',
+      reference: a4
+    },
+    {
+      labelid: 4,
+      key: 'answer5',
+      reference: a5
+    },
+    {
+      labelid: 5,
+      key: 'answer6',
+      reference: a6
+    },
+    {
+      labelid: 6,
+      key: 'answer7',
+      reference: a7
+    },]
  
     return(
         <div className="container">
@@ -110,17 +80,25 @@ function Question (props) {
     data && data.questions.length > 0 &&
     <ol>
  
-      <div className='question_text'>{data.questions[currentquestion].question}</div>
-       <div className='answer_section'>
-           <button onClick={answersYes} className='questionBox'>Yes</button>
-           <button onClick={answersNo} className='questionBox'>No</button>
-           <p>You answered {answer[data.questions[currentquestion].id]}</p>
-       </div>
-       <div>
-       <button onClick={() => previous()}>previous question</button>
-       <button onClick={() => next()}>Next question</button>
+      <div className='question_text'>{data.questions.allQuestions}</div>
+        <form>
+          
+          <div className='answer_section'>
+              {formElements.map(formElement => {
+                return <div>
+                  {data.questions[formElement.labelid].question}<br/>
+                  <label>Yes:
+                  <input type="radio" id="true" name="isTrue" value={true} ref={formElement.reference}/>
+                  </label>
+                  <label>No:
+                  <input type="radio" id="false" name="isFalse" value={false} ref={formElement.reference}/>
+                  </label>
+                  </div>
+              })}
+          </div>
+        </form>
        
-       </div>
+
    
       </ol>
     
